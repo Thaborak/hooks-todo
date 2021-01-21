6219580203a7fe3a8d3e7de7f86a3795513fbb97
Tom <thaborak@gmail.com>
2021-01-21 13:44:14 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
 
 function App() {
+  const [todoItems, setItems] = useState([])
+  const [input, setInput] = useState('')
+  const deleteItem = (i) => {
+    const temp = [...todoItems]
+    temp.splice(i, 1)
+    // console.log(temp)
+    setItems(temp)
+  }
+
+  const editItem = (i) => {
+    const temp = [...todoItems]
+    temp.splice(i, 1, input)
+    // console.log(temp)
+    setItems(temp)
+  }
+  console.log(todoItems.length)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <h1>Todo App</h1>
+      <input  onChange={(e) => setInput(e.target.value)} />
+      <input type="button" onClick={() => setItems([...todoItems, input])} ></input>
+
+      {
+
+        todoItems.map((t, i) => {
+          return <ul key={i}>({i}): {t} <form>Delete Item: <input type="button" onClick={() => deleteItem(i)} /> Edit Item: <input type="button" onClick={() => editItem(i)} /></form></ul> 
+        })
+      }
+    </div >
+
   );
 }
 
